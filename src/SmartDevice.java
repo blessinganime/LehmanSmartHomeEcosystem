d28b5fed45d663dd5a37ab8c80f24633a4786f34
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 21:47:12 -0500

    Refactor SmartDevice to be an abstract class
    Refactored SmartDevice class to be abstract and added a constructor.

--- src/SmartDevice.java
+++ src/SmartDevice.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public abstract class SmartDevice implements Powerable{
+    protected String deviceName;
+    protected boolean isOn;
 
-public class SmartDevice {
+    public SmartDevice(String deviceName){
+        this.deviceName = deviceName;
+        this.isOn = false;
+    }
+
+    static int activeDeviceCount = 0;
+
+    abstract void performSelfDiagnostic();
+
 }
