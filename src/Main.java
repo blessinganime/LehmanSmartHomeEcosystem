797c2f208937608de7e7213642de254ef120ae57
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 21:45:46 -0500

    Implement main method for smart home hub functionality
    Added main method to initialize smart home devices and perform actions.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<SmartDevice> homeHub = new ArrayList<>();
+        SmartLight livingRoom = new SmartLight("Smart light");
+        SmartLight kitchen = new SmartLight("Smart Light");
+        SmartThermostat Hallway = new SmartThermostat("Thermostat");
+
+        homeHub.add(livingRoom);
+        homeHub.add(kitchen);
+        homeHub.add(Hallway);
+
+        livingRoom.turnOn();
+        kitchen.turnOn();
+
+        Hallway.setLevel(50);
+
+        for(SmartDevice tech : homeHub){
+            tech.performSelfDiagnostic();
+        }
+    }
 }
