aad1a5d40458d7dc0e6fdf8bdac70a38a4706ea9
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 21:48:26 -0500

    Implement SmartThermostat with adjustable temperature
    Refactor SmartThermostat class to implement Adjustable interface and add temperature control methods.

--- src/SmartThermostat.java
+++ src/SmartThermostat.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+public class SmartThermostat extends SmartDevice implements Adjustable{
+    private int temperature;
 
-public class smartThermostat {
+    public SmartThermostat(String deviceName){
+        super(deviceName);
+        this.temperature = temperature;
+    }
+
+    @Override
+    public void turnOn() {// to turn the light on the light have to be off
+        if (!isOn) {// if smart light is not on then set it to tru to turn on
+            isOn = true;
+            activeDeviceCount++;
+        }
+    }
+
+    @Override
+    public void turnOff() {
+        if (isOn) {
+            isOn = false;
+            activeDeviceCount--;
+        }
+    }
+
+    @Override
+    public void setLevel(int level) {
+        if(!isOn){
+            System.out.println("Cannot adjust: Device is OFF.");
+        }
+
+        temperature = level;
+        System.out.println(deviceName + "temperature is set to " + temperature + "c");
+    }
+
+
+    @Override
+    void performSelfDiagnostic() {
+    }
 }
